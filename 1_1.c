26451741c0a59618b5d7b64ecad563c1cb5207fb
Andre Baptista <albs_br@yahoo.com.br>
2022-05-12 00:06:38 -0300

    v.0.5.1

--- 1_1.c
+++ 1_1.c
@@ -1,4 +1,33 @@
 /*
-Is Unique: Implement an algorithm to determine if a string has all unique characters. What if you
-cannot use additional data structures? 
+    Is Unique: Implement an algorithm to determine if a string has all unique characters. What if you
+    cannot use additional data structures? 
 */
+
+#include <stdio.h>
+#include <string.h>
+
+int IsUnique(char input[])
+{
+    for (int i = 0; i < strlen(input); i++)
+    {
+        for (int j = 0; j < strlen(input); j++)
+        {
+            if((j != i) && (input[i] != ' ') && (input[i] == input[j])) return 0;
+        }
+    }
+    
+    return 1;
+}
+
+int main(void)
+{
+    char input_1[] = "my string test";
+    char input_2[] = "the quick brown fox";
+    char input_3[] = "the quick brown";
+
+    printf("%i\r\n", IsUnique(input_1));
+    printf("%i\r\n", IsUnique(input_2));
+    printf("%i\r\n", IsUnique(input_3));
+
+    return 0;
+}
