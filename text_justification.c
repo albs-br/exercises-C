ef6f636ab62bcfa0903d49cf5cfdffb3cb6d1942
Andre Baptista <albs_br@yahoo.com.br>
2022-05-13 23:40:52 -0300

    v.0.11.1

--- text_justification.c
+++ text_justification.c
@@ -6,6 +6,7 @@
 int main()
 {
     // inputs (text and maxLineLength)
+    //const int NUMBER_OF_WORDS = 7;
     const char *a[7];
     a[0] = "Text";
     a[1] = "justification";
@@ -15,31 +16,38 @@ int main()
     a[5] = "words";
     a[6] = "here";
     const int maxLineLength = 20;
-    
+
+    char output[200] = "";
+
+    // draw "ruler"
+    for(int i=0; i<maxLineLength; i++)
+    {
+        printf("%i", (i%10));
+    }
+    printf("\r\n");
+
+
     int lineLength = 0;
-    int numberOfSpaces = 0;
     for(int i=0; i<7; i++)
     {
-        
-        //printf("%i\r\n", lineLength);
+        //printf("%s ", a[i]);
 
-        if((lineLength + strlen(a[i]) + numberOfSpaces) > maxLineLength)
+        int spaceBefore = (strlen(output) > 0) ? 1 : 0;
+
+        if((lineLength + strlen(a[i]) + spaceBefore) > maxLineLength)
         {
-            printf("lineLength: %i\r\n", lineLength);
-            printf("numberOfSpaces: %i\r\n", numberOfSpaces);
-            
-            int requiredSpaces = maxLineLength - (lineLength + numberOfSpaces);
-            
+            printf("%s\r\n", output);
+            printf("spaces left to fill line: %i\r\n", maxLineLength - lineLength);
+
+            // loop through current line filling the missing spaces on current spaces, left to right
+
             lineLength = 0;
-            numberOfSpaces = 0;
-            printf("%i\r\n", requiredSpaces);
+            output[0] = '\0';
         }
-        else
-        {
-            numberOfSpaces++;
-            lineLength += strlen(a[i]);
-        }
-        printf("%s,", a[i]);
+
+        lineLength += strlen(a[i]) + spaceBefore;
+        if(spaceBefore == 1) strcat(output, " ");
+        strcat(output, a[i]);
     }
 
     return 0;
